b6def79fc3bac85722c653c0ca4ad4ee6d9a733e
BlindDev-cloud <korsikoff@gmail.com>
2022-09-02 22:53:30 +0300

    [task_20] Add functions into Regex class

--- task_20/index.php
+++ task_20/index.php
@@ -1,5 +1,13 @@
 <?php
 
-require_once __DIR__.'/Classes/Regex.php';
+require_once __DIR__ . '/Classes/Regex.php';
 
 use Classes\Regex;
+
+$regex = new Regex();
+$text = 'The quick brown fox';
+echo $regex->replaceLastWord($text) . PHP_EOL;
+echo $regex->replaceSpaces($text) . PHP_EOL;
+echo $regex->replaceNonNumbers('$123,3W4.00A') . PHP_EOL;
+echo $regex->replaceNonInParenthesesText('The quick brown [fox]') . PHP_EOL;
+echo $regex->replaceNonLetters('abcde$ddfd @abcd )der]') . PHP_EOL;
