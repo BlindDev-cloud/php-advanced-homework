7287ae2f918697afd725e003e511d93325fdd1d5
BlindDev-cloud <korsikoff@gmail.com>
2022-10-08 17:39:34 +0300

    [task_14] Did refactoring of class Product

--- task_14/Classes/Product.php
+++ task_14/Classes/Product.php
@@ -5,9 +5,4 @@ namespace Classes;
 class Product {
     public function get($name) {}
     public function set($name, $value) {}
-    public function save() {}
-    public function update() {}
-    public function delete() {}
-    public function show() {}
-    public function print() {}
 }
---
+++ task_14/Classes/ProductCRUD.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Classes;
+
+class ProductCRUD
+{
+    public function save() {}
+    public function update() {}
+    public function delete() {}
+    public function show() {}
+}
