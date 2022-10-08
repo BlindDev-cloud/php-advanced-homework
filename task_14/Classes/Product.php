043739809d0d348b9a83242d3fb11a19cf2ca1f2
BlindDev-cloud <korsikoff@gmail.com>
2022-10-08 17:32:22 +0300

    [task_14] Created class Product

---
+++ task_14/Classes/Product.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Classes;
+
+class Product {
+    public function get($name) {}
+    public function set($name, $value) {}
+    public function save() {}
+    public function update() {}
+    public function delete() {}
+    public function show() {}
+    public function print() {}
+}
