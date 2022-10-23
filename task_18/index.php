b736a5a3b1f821558f105c03e97991a3158f71f8
BlindDev-cloud <korsikoff@gmail.com>
2022-10-23 17:07:19 +0300

    [task_18] Created index.php with some classes

---
+++ task_18/index.php
@@ -0,0 +1,24 @@
+<?php
+
+class Mysql
+{
+    public function getData()
+    {
+        return 'some data from database';
+    }
+}
+
+class Controller
+{
+    private $adapter;
+
+    public function __construct(Mysql $mysql)
+    {
+        $this->adapter = $mysql;
+    }
+
+    function getData()
+    {
+        $this->adapter->getData();
+    }
+}
