10886e4d742980fff825b79e599f2b82d20d51fe
BlindDev-cloud <korsikoff@gmail.com>
2022-10-23 15:58:00 +0300

    [task_17] Created index.php with classes and interface

---
+++ task_17/index.php
@@ -0,0 +1,19 @@
+<?php
+
+interface Bird
+{
+    public function eat();
+    public function fly();
+}
+
+class Swallow implements Bird
+{
+    public function eat() { ... }
+    public function fly() { ... }
+}
+
+class Ostrich implements Bird
+{
+    public function eat() { ... }
+    public function fly() { /* exception */ }
+}
